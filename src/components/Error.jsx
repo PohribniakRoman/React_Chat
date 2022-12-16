c975b3f27151b6a414a8886c3a8c4905892d34cc
Roman <roman.pohribniak@gmail.com>
2022-12-16 20:48:09 +0200

    fix routes err

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export default function Error() {
+    return <>
+        ugh,page not found,linke to homepage is <Link to={"/React_Chat"}>here</Link>
+    </>
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,6 @@
 import 'react-router-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Error from './components/Error';
 import Home from './components/Home';
 import Room from './components/Room';
 
@@ -7,8 +8,9 @@ export default function Router() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/:roomId' element={<Room/>}/>
-                <Route excat path='/' element={<Home/>}/>
+                <Route path='React_Chat/:roomId' element={<Room/>}/>
+                <Route excat path='React_Chat/' element={<Home/>}/>
+                <Route path="*" element={<Error/>}/>
             </Routes>
         </BrowserRouter>
     )
